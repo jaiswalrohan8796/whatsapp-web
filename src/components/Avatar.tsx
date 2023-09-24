7b3f974b28255329473502e4c249b93d8bb85dd1
Rohan Jaiswal <rohan@fitpage.in>
2023-09-24 15:51:00 +0530

    Avatar fix

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 interface IAvatar {
 	classNames?:string,
 	height?: number,
 	width?: number
 }
 
-const Avatar:React.FC = ({classNames = "", height = 44, width = 44}) => {
+const Avatar = ({classNames = "", height = 44, width = 44}: IAvatar) : React.JSX.Element => {
 	return <div><Image className={`text-mainLight cursor-pointer ${classNames}`} src={"/icons/avatar.svg"} height={height} width={width} alt={"Avatar"} onClick={() => {}} />
 	</div>
 }
